f5461c3ac73b6597920dcb221e494e83462549fc
olaniSiyum <olisiyum@gmail.com>
2023-12-04 01:02:50 +0300

    terraform for deploying ec2

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-2"  # Change this to your desired AWS region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"  
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorldInstance"
+  }
+}
